77c1e9e1c15fa1efe891489bebd2dc7140a422eb
harshagns19 <57993831+harshagns19@users.noreply.github.com>
2020-09-15 15:56:33 +0200

    Update checkStatus.sh

--- checkStatus.sh
+++ checkStatus.sh
@@ -1,2 +1,4 @@
-responseCode=curl -o /dev/null -s -w "%{http_code}\n" http://localhost:8080/GLPProject-0.0.1-SNAPSHOT/
+responseCode=$(curl -o /dev/null -s -w "%{http_code}\n" http://localhost:8080/GLPProject-0.0.1-SNAPSHOT/)
+
 echo "response code is "+$responseCode
+
