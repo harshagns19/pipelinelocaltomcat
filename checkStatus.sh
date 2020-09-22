6bb499bccd52e4845934f98e6619e72ee0503804
harshagns19 <57993831+harshagns19@users.noreply.github.com>
2020-09-22 10:48:43 +0200

    Update checkStatus.sh

--- checkStatus.sh
+++ checkStatus.sh
@@ -1,13 +1,7 @@
-echo "================================From checkStatus.sh File ========================================================="
-echo "sleep fpr 10 secs so the application will be up and runninf after the deployment"
+echo "=============================== From checkStatus.sh File ========================================================="
+echo "================ sleep fpr 10 secs so the application will be up and runninf after the deployment ================"
 sleep 10s
-response=sh(script: "curl -s $url", returnStdout: true).trim()
-echo "response is "+$response;
-if (response == *"Welcome")
-	echo "Deployment Successful and Application is up and Running!"
-else
-	echo "Deployment Failed!!!"
-fi
+
 responseCode=$(curl -o /dev/null -s -w "%{http_code}\n" http://localhost:8080/GLPProject-0.0.1-SNAPSHOT/)
 
 echo "response code is "+$responseCode
