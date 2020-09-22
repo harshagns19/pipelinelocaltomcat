ba0f27336e39c32f057e7c89e47fe912dac4309f
harshagns19 <57993831+harshagns19@users.noreply.github.com>
2020-09-22 14:56:42 +0200

    Update checkStatus.sh

--- checkStatus.sh
+++ checkStatus.sh
@@ -1,7 +1,7 @@
 echo "=============================== From checkStatus.sh File ========================================================="
 echo "================ Sleep for 10 secs so the application will be up and running after the deployment ================"
 sleep 10s
-echo $PWD
+echo "================================ Working Directory of Jenkins is ==============================="+$PWD
 responseCode=$(curl -o /dev/null -s -w "%{http_code}\n" http://localhost:8080/GLPProject-0.0.1-SNAPSHOT/)
 
 echo "response code is "+$responseCode
