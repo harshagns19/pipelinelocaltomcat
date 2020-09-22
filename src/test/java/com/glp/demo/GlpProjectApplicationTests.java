7ea77364a34f566a6e62f890eeefcffa0806d6a1
harshagns19 <57993831+harshagns19@users.noreply.github.com>
2020-09-22 14:15:04 +0200

    Added the selenium test for my App

--- src/test/java/com/glp/demo/GlpProjectApplicationTests.java
+++ src/test/java/com/glp/demo/GlpProjectApplicationTests.java
@@ -1,13 +1,29 @@
 package com.glp.demo;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GlpProjectApplicationTests {
-
+	
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	public void openURL() {
+		System.out.println("Inside open url");
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\Naga.Gundabathula\\Downloads\\geckodriver-v0.27.0-win64\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		
+		driver.manage().window().maximize();
+		
+		driver.get("http://localhost:8080/GLPProject-0.0.1-SNAPSHOT/");
+		driver.findElement(By.xpath("//a[text()='Click Me']")).click();
+		System.out.println("After testing");
 	}
 
 }
